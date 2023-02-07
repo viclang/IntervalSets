95ca9c857f3f709e04829958274a376b44579a0f
viclang <viclang.github@pm.me>
2023-02-07 23:02:51 +0100

    Documentation for Iterator

--- src/IntervalRecord/Iterator.cs
+++ src/IntervalRecord/Iterator.cs
@@ -4,6 +4,13 @@ namespace IntervalRecord
 {
     public static partial class Interval
     {
+        /// <summary>
+        /// Generates a sequence of values within the interval by incrementing the starting value using the given step function.
+        /// </summary>
+        /// <typeparam name="T">The type of the interval endpoints.</typeparam>
+        /// <param name="source">The interval to generate values from.</param>
+        /// <param name="addStep">The step function to increment the starting value with.</param>
+        /// <returns>The sequence of values generated within the interval.</returns>
         [Pure]
         public static IEnumerable<T> Iterate<T>(this Interval<T> source, Func<T, T> addStep)
             where T : struct, IEquatable<T>, IComparable<T>, IComparable
@@ -16,6 +23,14 @@ namespace IntervalRecord
             return source.Iterate(start, addStep);
         }
 
+        /// <summary>
+        /// Generates a sequence of values within the interval by incrementing the starting value using the given step function.
+        /// </summary>
+        /// <typeparam name="T">The type of the interval endpoints.</typeparam>
+        /// <param name="source">The interval to generate values from.</param>
+        /// <param name="start">The starting value of the sequence.</param>
+        /// <param name="addStep">The step function to increment the starting value with.</param>
+        /// <returns>The sequence of values generated within the interval.</returns>
         [Pure]
         public static IEnumerable<T> Iterate<T>(this Interval<T> source, T start, Func<T, T> addStep)
             where T : struct, IEquatable<T>, IComparable<T>, IComparable
