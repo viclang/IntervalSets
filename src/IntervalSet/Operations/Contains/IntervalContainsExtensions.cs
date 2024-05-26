6de151027e5dae0d7f0fd6c16cba0ce3f18b3035
viclang <viclang.github@pm.me>
2024-05-26 19:19:31 +0200

    Contains rename other to value

--- src/IntervalSet/Operations/Contains/IntervalContainsExtensions.cs
+++ src/IntervalSet/Operations/Contains/IntervalContainsExtensions.cs
@@ -3,70 +3,70 @@
 namespace IntervalSet.Operations;
 public static class IntervalContainsExtensions
 {
-    public static bool Contains<T>(this IInterval<T> interval, T other)
+    public static bool Contains<T>(this IInterval<T> interval, T value)
         where T : notnull, IComparable<T>, ISpanParsable<T>
     {
-        var startComparison = interval.StartBound.IsUnbounded() ? -1 : interval.Start.CompareTo(other);
-        var endComparison = interval.EndBound.IsUnbounded() ? -1 : other.CompareTo(interval.End);
+        var startComparison = interval.StartBound.IsUnbounded() ? -1 : interval.Start.CompareTo(value);
+        var endComparison = interval.EndBound.IsUnbounded() ? -1 : value.CompareTo(interval.End);
 
         return startComparison < 0 && endComparison < 0
             || startComparison == 0 && interval.StartBound.IsClosed()
             || endComparison == 0 && interval.EndBound.IsClosed();
     }
 
-    public static bool Contains<T>(this Interval<T, Open, Open> interval, T other)
+    public static bool Contains<T>(this Interval<T, Open, Open> interval, T value)
         where T : IComparable<T>, ISpanParsable<T>
     {
-        return interval.Start.CompareTo(other) < 0
-            && other.CompareTo(interval.End) < 0;
+        return interval.Start.CompareTo(value) < 0
+            && value.CompareTo(interval.End) < 0;
     }
 
-    public static bool Contains<T>(this Interval<T, Closed, Open> interval, T other)
+    public static bool Contains<T>(this Interval<T, Closed, Open> interval, T value)
         where T : IComparable<T>, ISpanParsable<T>
     {
-        return interval.Start.CompareTo(other) <= 0
-            && other.CompareTo(interval.End) < 0;
+        return interval.Start.CompareTo(value) <= 0
+            && value.CompareTo(interval.End) < 0;
     }
 
-    public static bool Contains<T>(this Interval<T, Open, Closed> interval, T other)
+    public static bool Contains<T>(this Interval<T, Open, Closed> interval, T value)
         where T : IComparable<T>, ISpanParsable<T>
     {
-        return interval.Start.CompareTo(other) < 0
-            && other.CompareTo(interval.End) <= 0;
+        return interval.Start.CompareTo(value) < 0
+            && value.CompareTo(interval.End) <= 0;
     }
 
-    public static bool Contains<T>(this Interval<T, Closed, Closed> interval, T other)
+    public static bool Contains<T>(this Interval<T, Closed, Closed> interval, T value)
         where T : IComparable<T>, ISpanParsable<T>
     {
-        return interval.Start.CompareTo(other) <= 0
-            && other.CompareTo(interval.End) <= 0;
+        return interval.Start.CompareTo(value) <= 0
+            && value.CompareTo(interval.End) <= 0;
     }
 
-    public static bool Contains<T>(this Interval<T, Open, Unbounded> interval, T other)
+    public static bool Contains<T>(this Interval<T, Open, Unbounded> interval, T value)
         where T : IComparable<T>, ISpanParsable<T>
     {
-        return interval.Start.CompareTo(other) < 0;
+        return interval.Start.CompareTo(value) < 0;
     }
 
-    public static bool Contains<T>(this Interval<T, Closed, Unbounded> interval, T other)
+    public static bool Contains<T>(this Interval<T, Closed, Unbounded> interval, T value)
         where T : IComparable<T>, ISpanParsable<T>
     {
-        return interval.Start.CompareTo(other) <= 0;
+        return interval.Start.CompareTo(value) <= 0;
     }
 
-    public static bool Contains<T>(this Interval<T, Unbounded, Open> interval, T other)
+    public static bool Contains<T>(this Interval<T, Unbounded, Open> interval, T value)
         where T : IComparable<T>, ISpanParsable<T>
     {
-        return other.CompareTo(interval.End) < 0;
+        return value.CompareTo(interval.End) < 0;
     }
 
-    public static bool Contains<T>(this Interval<T, Unbounded, Closed> interval, T other)
+    public static bool Contains<T>(this Interval<T, Unbounded, Closed> interval, T value)
         where T : IComparable<T>, ISpanParsable<T>
     {
-        return other.CompareTo(interval.End) <= 0;
+        return value.CompareTo(interval.End) <= 0;
     }
 
-    public static bool Contains<T>(this Interval<T, Unbounded, Unbounded> interval, T other)
+    public static bool Contains<T>(this Interval<T, Unbounded, Unbounded> _, T __)
         where T : IComparable<T>, ISpanParsable<T>
     {
         return true;
